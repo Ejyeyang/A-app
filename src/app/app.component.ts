04cf8e82a160e248e384ef09b36df00cdb5bf683
Ejyeyang <75270307+Ejyeyang@users.noreply.github.com>
2021-05-26 08:39:03 -0700

    generates random pw

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,8 +7,54 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   password: string = "";
+  includeLetters = false;
+  includeNumbers = false;
+  includeSymbols = false;
+  validChars: string = "";
+  length: number;
 
   onSubmit(){
-    this.password = "submitted";
+    const numbers = '1234567890';
+    const letters = 'abcdefghijklmnopqrstuvwxyz';
+    const symbols = '!@#$%^&*()';
+    if(this.includeLetters){
+      this.validChars += letters;
+    }
+    if(this.includeNumbers){
+      this.validChars += numbers;
+    }
+    if(this.includeSymbols){
+      this.validChars += symbols;
+    }
+
+    let generatedPassword = "";
+    for(let i = 0; i < this.length; i++){
+      const index = Math.floor(Math.random() * this.validChars.length);
+      generatedPassword += this.validChars[index];
+    }
+    this.password = generatedPassword;
   }
+
+  onChangeLength(value: string){
+    const parsedValue = parseInt(value);
+    if(!isNaN(parsedValue)){
+      this.length = parseInt(value);
+    }
+  }
+
+  onChangeUseLetters(){
+    this.includeLetters = !this.includeLetters;
+  }
+
+  onChangeUseNumbers(){
+    this.includeNumbers = !this.includeNumbers;
+  }
+
+  onChangeUseSymbols(){
+    this.includeSymbols = !this.includeSymbols;
+  }
+
+  // getPassword(){
+  //   return this.password;
+  // }
 }
