f0b91e1ae7212f1652fc19beaa2193a3c0360410
Ejyeyang <75270307+Ejyeyang@users.noreply.github.com>
2021-05-26 08:09:39 -0700

    propertyBinding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,9 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'A-app';
+  password: string = "";
 
   onSubmit(){
-    console.log("submitted");
+    this.password = "submitted";
   }
 }
