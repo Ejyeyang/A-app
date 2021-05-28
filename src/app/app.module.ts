85d67d35f1711c2f4047216d50bc5291f355ae14
Ejyeyang <75270307+Ejyeyang@users.noreply.github.com>
2021-05-28 09:50:07 -0700

    added a router to project to navigate

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CardsComponent } from './cards/cards.component';
+import { NavComponent } from './nav/nav.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardsComponent,
+    NavComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
